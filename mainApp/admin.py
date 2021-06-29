8ca7a69f00015bbb66c8fc576df8da92ff692e53
fabriccio27 <fabricio_inc27@hotmail.com>
2021-06-29 10:40:11 -0300

    customized a bit of the admin page

--- mainApp/admin.py
+++ mainApp/admin.py
@@ -1,4 +1,30 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 # Register your models here.
-admin.site.register(Question)
+
+""" By registering the Question model with admin.site.register(Question), Django was able to construct a default form 
+representation. Often, you’ll want to customize how the admin form looks and works. 
+You’ll do this by telling Django the options you want when you register the object. """
+
+#podria poner admin.site.register(Choice) pero me daria una pagina donde agrego una choice 
+#y tengo que vincularla a una question manualmente. Seria mejor que cuando cree question ya me de para agregarle choices
+
+#otro modo de layout es StackedInline, pero ocupa mucho espacio
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3 
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date'] #como en models puse que es un DatetimeField, django sabe que filtros poner en la pagina de admin
+    """ search_fields = ['question_text'] funciona como uno intuye """
+    fieldsets = [
+        #primer argumento de la tupla es titulo de la mini seccion dentro de esta pagina de admin
+        (None, {'fields':['question_text']}),
+        ('Date information', {'fields':['pub_date'], 'classes':['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
+ 
--- mainApp/models.py
+++ mainApp/models.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 
 # Create your models here.
@@ -11,6 +12,12 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    ) #esto me cambia el display en la pagina de admin, description va a ser el titulo de la columna si esta como TabularInline
 
     def was_published_recently(self):
         # si de alguna manera engaño para que pub_date sea en el futuro, para bocha de dias me va a dar true esto
